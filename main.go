9db98baab25e224756d1d10de6b09e77cbcc8b1e
munnik <martijndemunnik@protonmail.com>
2021-04-09 14:13:44 +0200

    Functions to create and compare binary trees

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"container/list"
+	"fmt"
+)
 
 type ListNode struct {
 	Val  int
@@ -23,4 +26,90 @@ func CreateLinkedList(number int) *ListNode {
 	}
 
 	return result
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func (t *TreeNode) Equals(other *TreeNode) bool {
+	if t.Val != other.Val {
+		return false
+	}
+	result := true
+	if t.Left != nil || other.Left != nil {
+		if t.Left == nil && other.Left != nil {
+			result = false
+		} else if t.Left != nil && other.Left == nil {
+			result = false
+		} else {
+			result = result && t.Left.Equals(other.Left)
+		}
+	}
+	if t.Right != nil || other.Right != nil {
+		if t.Right == nil && other.Right != nil {
+			result = false
+		} else if t.Right != nil && other.Right == nil {
+			result = false
+		} else {
+			result = result && t.Right.Equals(other.Right)
+		}
+	}
+
+	return result
+}
+
+// CreateBinaryTree returns the root node of a binary tree created from the input, a negative number in the input represents a nil node
+func CreateBinaryTree(numbers []int) *TreeNode {
+	if len(numbers) == 0 {
+		return nil
+	}
+	queue := list.New()
+
+	root := &TreeNode{Val: numbers[0]}
+	queue.PushBack(root)
+
+	for i := 1; i < len(numbers); i++ {
+		e := queue.Front()
+		if n, ok := e.Value.(*TreeNode); ok {
+			if n.Left == nil {
+				n.Left = &TreeNode{Val: numbers[i]}
+				if n.Left.Val >= 0 {
+					queue.PushBack(n.Left)
+				}
+			} else {
+				n.Right = &TreeNode{Val: numbers[i]}
+				if n.Right.Val >= 0 {
+					queue.PushBack(n.Right)
+				}
+				queue.Remove(e)
+			}
+		}
+	}
+
+	queue = list.New()
+	queue.PushBack(root)
+	for e := queue.Front(); e != nil; e = queue.Front() {
+		if n, ok := e.Value.(*TreeNode); ok {
+			if n.Left != nil {
+				if n.Left.Val == -1 {
+					n.Left = nil
+				} else {
+					queue.PushBack(n.Left)
+				}
+			}
+			if n.Right != nil {
+				if n.Right.Val == -1 {
+					n.Right = nil
+				} else {
+					queue.PushBack(n.Right)
+				}
+			}
+		}
+		queue.Remove(e)
+	}
+
+	return root
 }
---
+++ main_test.go
@@ -0,0 +1,235 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestListNodeString(t *testing.T) {
+	tests := []struct {
+		input *ListNode
+		want  string
+	}{
+		{&ListNode{Val: 2, Next: &ListNode{Val: 4, Next: &ListNode{Val: 3}}}, "2 > 4 > 3"},
+		{&ListNode{}, "0"},
+		{&ListNode{Val: 0}, "0"},
+		{&ListNode{Val: 8, Next: &ListNode{Val: 9, Next: &ListNode{Val: 9, Next: &ListNode{Val: 9, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 0, Next: &ListNode{Val: 1}}}}}}}}, "8 > 9 > 9 > 9 > 0 > 0 > 0 > 1"},
+	}
+
+	for i, testCase := range tests {
+		t.Run(
+			fmt.Sprintf("Test %d", i),
+			func(t *testing.T) {
+				got := testCase.input
+				if got.String() != testCase.want {
+					t.Fatalf("Got %v; want %v; with input %v", got, testCase.want, testCase.input)
+				} else {
+					t.Logf("Success!")
+				}
+			},
+		)
+	}
+}
+
+func TestCreateLinkedList(t *testing.T) {
+	tests := []struct {
+		input int
+		want  string
+	}{
+		{342, "2 > 4 > 3"},
+		{0, "0"},
+		{10009998, "8 > 9 > 9 > 9 > 0 > 0 > 0 > 1"},
+	}
+
+	for i, testCase := range tests {
+		t.Run(
+			fmt.Sprintf("Test %d", i),
+			func(t *testing.T) {
+				got := CreateLinkedList(testCase.input)
+				if got.String() != testCase.want {
+					t.Fatalf("Got %v; want %v; with input %v", got, testCase.want, testCase.input)
+				} else {
+					t.Logf("Success!")
+				}
+			},
+		)
+	}
+}
+
+func TestTreeNodeEquals(t *testing.T) {
+	tests := []struct {
+		input []*TreeNode
+		want  bool
+	}{
+		{
+			[]*TreeNode{
+				{Val: 0},
+				{Val: 1},
+			},
+			false,
+		},
+		{
+			[]*TreeNode{
+				{Val: 1},
+				{Val: 1},
+			},
+			true,
+		},
+		{
+			[]*TreeNode{
+				{Val: 1, Left: &TreeNode{Val: 2}},
+				{Val: 1},
+			},
+			false,
+		},
+		{
+			[]*TreeNode{
+				{Val: 1},
+				{Val: 1, Left: &TreeNode{Val: 2}},
+			},
+			false,
+		},
+		{
+			[]*TreeNode{
+				{Val: 1, Left: &TreeNode{Val: 2}},
+				{Val: 1, Left: &TreeNode{Val: 1}},
+			},
+			false,
+		},
+		{
+			[]*TreeNode{
+				{Val: 1, Left: &TreeNode{Val: 2}},
+				{Val: 1, Left: &TreeNode{Val: 2}},
+			},
+			true,
+		},
+		{
+			[]*TreeNode{
+				{Val: 1, Left: &TreeNode{Val: 2}, Right: &TreeNode{Val: 3}},
+				{Val: 1, Left: &TreeNode{Val: 2}},
+			},
+			false,
+		},
+		{
+			[]*TreeNode{
+				{Val: 1, Left: &TreeNode{Val: 2}, Right: &TreeNode{Val: 3}},
+				{Val: 1, Left: &TreeNode{Val: 2}, Right: &TreeNode{Val: 0}},
+			},
+			false,
+		},
+		{
+			[]*TreeNode{
+				{Val: 1, Left: &TreeNode{Val: 2}, Right: &TreeNode{Val: 3}},
+				{Val: 1, Left: &TreeNode{Val: 2}, Right: &TreeNode{Val: 3}},
+			},
+			true,
+		},
+		{
+			[]*TreeNode{
+				{Val: 1, Left: &TreeNode{Val: 2}, Right: &TreeNode{Val: 3, Right: &TreeNode{Val: 4}}},
+				{Val: 1, Left: &TreeNode{Val: 2}, Right: &TreeNode{Val: 3}},
+			},
+			false,
+		},
+		{
+			[]*TreeNode{
+				{Val: 1, Left: &TreeNode{Val: 2}, Right: &TreeNode{Val: 3}},
+				{Val: 1, Left: &TreeNode{Val: 2}, Right: &TreeNode{Val: 3, Right: &TreeNode{Val: 4}}},
+			},
+			false,
+		},
+		{
+			[]*TreeNode{
+				{Val: 1, Left: &TreeNode{Val: 2}, Right: &TreeNode{Val: 3, Right: &TreeNode{Val: 4}}},
+				{Val: 1, Left: &TreeNode{Val: 2}, Right: &TreeNode{Val: 3, Right: &TreeNode{Val: 2}}},
+			},
+			false,
+		},
+		{
+			[]*TreeNode{
+				{Val: 1, Left: &TreeNode{Val: 2}, Right: &TreeNode{Val: 3, Right: &TreeNode{Val: 4}}},
+				{Val: 1, Left: &TreeNode{Val: 2}, Right: &TreeNode{Val: 3, Right: &TreeNode{Val: 4}}},
+			},
+			true,
+		},
+		{
+			[]*TreeNode{
+				{Val: 0, Left: &TreeNode{Val: 2}, Right: &TreeNode{Val: 3, Right: &TreeNode{Val: 4}}},
+				{Val: 1, Left: &TreeNode{Val: 2}, Right: &TreeNode{Val: 3, Right: &TreeNode{Val: 4}}},
+			},
+			false,
+		},
+		{
+			[]*TreeNode{
+				{Val: 1, Left: &TreeNode{Val: 2}, Right: &TreeNode{Val: 3, Right: &TreeNode{Val: 4}}},
+				{Val: 1, Left: &TreeNode{Val: 2}, Right: &TreeNode{Val: 6, Right: &TreeNode{Val: 4}}},
+			},
+			false,
+		},
+	}
+
+	for i, testCase := range tests {
+		t.Run(
+			fmt.Sprintf("Test %d", i),
+			func(t *testing.T) {
+				got := testCase.input[0].Equals(testCase.input[1])
+				if got != testCase.want {
+					t.Fatalf("Got %v; want %v; with input %v", got, testCase.want, testCase.input)
+				} else {
+					t.Logf("Success!")
+				}
+			},
+		)
+	}
+
+}
+
+func TestCreateBinaryTree(t *testing.T) {
+	tests := []struct {
+		input []int
+		want  *TreeNode
+	}{
+		{
+			[]int{},
+			nil,
+		},
+		{
+			[]int{0},
+			&TreeNode{Val: 0},
+		},
+		{
+			[]int{0, 1},
+			&TreeNode{Val: 0, Left: &TreeNode{Val: 1}},
+		},
+		{
+			[]int{0, 1, 2},
+			&TreeNode{Val: 0, Left: &TreeNode{Val: 1}, Right: &TreeNode{Val: 2}},
+		},
+		{
+			[]int{0, 1, 2, 3},
+			&TreeNode{Val: 0, Left: &TreeNode{Val: 1, Left: &TreeNode{Val: 3}}, Right: &TreeNode{Val: 2}},
+		},
+		{
+			[]int{0, 1, 2, -1, 3},
+			&TreeNode{Val: 0, Left: &TreeNode{Val: 1, Right: &TreeNode{Val: 3}}, Right: &TreeNode{Val: 2}},
+		},
+		{
+			[]int{1, 3, 4, 7, -1, 8, 2, 4, 3, 9, -1, 1, 7},
+			&TreeNode{Val: 1, Left: &TreeNode{Val: 3, Left: &TreeNode{Val: 7, Left: &TreeNode{Val: 4}, Right: &TreeNode{Val: 3}}}, Right: &TreeNode{Val: 4, Left: &TreeNode{Val: 8, Left: &TreeNode{Val: 9}}, Right: &TreeNode{Val: 2, Left: &TreeNode{Val: 1}, Right: &TreeNode{Val: 7}}}},
+		},
+	}
+
+	for i, testCase := range tests {
+		t.Run(
+			fmt.Sprintf("Test %d", i),
+			func(t *testing.T) {
+				got := CreateBinaryTree(testCase.input)
+				if got == nil && testCase.want != nil || got != nil && testCase.want == nil || !got.Equals(testCase.want) {
+					t.Fatalf("Got %v; want %v; with input %v", got, testCase.want, testCase.input)
+				} else {
+					t.Logf("Success!")
+				}
+			},
+		)
+	}
+}
